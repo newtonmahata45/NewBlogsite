e9525f38309e0573806fb3afd5f52c58749fb848
newtonmahata45 <newtonmahata45@gmail.com>
2023-07-20 00:24:04 +0530

    css added

--- client/blogsite/src/Createbog.jsx
+++ client/blogsite/src/Createbog.jsx
@@ -1,79 +1,82 @@
-import axios from 'axios';
+import axios from "axios";
 import jwt_decode from "jwt-decode";
-import React, { useState, useEffect } from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 let backendurl = "https://new-blogsite.vercel.app";
-                //'http://localhost:3001'
+//'http://localhost:3001'
 function Createblog() {
-    const navigate = useNavigate();
-    const [blog, setBlog] = useState({});
-    const decoded = jwt_decode(localStorage.getItem("token"));
+  const navigate = useNavigate();
+  const [blog, setBlog] = useState({});
+  const decoded = jwt_decode(localStorage.getItem("token"));
 
-    
-    async function submit(e) {
-        e.preventDefault();
-        const formData = new FormData()
-        formData.append("title",blog.title);
-        formData.append("description",blog.description);
-        if(blog.blogFile && blog.blogFile.length){
-          formData.append("blogFile",blog.blogFile[0]);
-        }
-        formData.append("userId",decoded.userId);
-        
-        axios.post( `${backendurl}/blogs`,
-           formData,
-           { headers: { "Content-Type": "multipart/form-data","x-api-key":`${localStorage.getItem("token")}` }}
-          )
-            .then(function (res) {
-                console.log(res);
-              window.alert(res.data.message)
-              navigate(`/blogpage/${res.data.data._id}`,{state:res.data.data});
-            })
-            .catch(function (err) {
-            //   window.alert(err.response.data.message)
-              console.log(err);
-            });
-
+  async function submit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", blog.title);
+    formData.append("description", blog.description);
+    if (blog.blogFile && blog.blogFile.length && typeof blog.blogFile != String) {
+      formData.append("blogFile", blog.blogFile[0]);
     }
-    
-    function handle(e) {
-        const newblog = { ...blog };
-        if(e.target.id == "blogFile" )newblog[e.target.id] = e.target.files;
-        else 
-            newblog[e.target.id] = e.target.value;
-        setBlog(newblog);
-        console.log(newblog);
-      }
-    return (
-        <div>
+    formData.append("userId", decoded.userId);
+
+    axios
+      .post(`${backendurl}/blogs`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "x-api-key": `${localStorage.getItem("token")}`,
+        },
+      })
+      .then(function (res) {
+        console.log(res);
+        window.alert(res.data.message);
+        navigate(`/blogpage/${res.data.data._id}`, { state: res.data.data });
+      })
+      .catch(function (err) {
+        //   window.alert(err.response.data.message)
+        console.log(err);
+      });
+  }
+
+  function handle(e) {
+    const newblog = { ...blog };
+    if (e.target.id == "blogFile") newblog[e.target.id] = e.target.files;
+    else newblog[e.target.id] = e.target.value;
+    setBlog(newblog);
+    console.log(newblog);
+  }
+  return (
+    <div>
       <form className="form" onSubmit={(e) => submit(e)}>
         <span>Title: </span>
         <input
-            onChange={(e) => handle(e)}
-            value={blog.title}
-            type="text"
-            placeholder="Title of the blog"
-            id="title"
-            required
-                /><br/>
+          onChange={(e) => handle(e)}
+          value={blog.title}
+          type="text"
+          placeholder="Title of the blog"
+          id="title"
+          required
+        />
+        <br />
         <textarea
-            onChange={(e) => handle(e)}
-            value={blog.description}
-            type="textarea"
-            placeholder="Write the content of the blog"
-            id="description"
-            required
-                /><br/>
+          onChange={(e) => handle(e)}
+          value={blog.description}
+          type="textarea"
+          placeholder="Write the content of the blog"
+          id="description"
+          required
+        />
+        <br />
         <input
-            onChange={(e) => handle(e)}
-            type="file"
-            placeholder="Choose File"
-            id="blogFile"
-                /><br/>
+          onChange={(e) => handle(e)}
+          type="file"
+          placeholder="Choose File"
+          id="blogFile"
+        />
+        <br />
         <button type="submit">Create</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Createblog
+export default Createblog;
--- client/blogsite/src/Home.jsx
+++ client/blogsite/src/Home.jsx
@@ -1,45 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import "./home.css";
 
 let backendurl = "https://new-blogsite.vercel.app";
 
 function Home() {
+  const [allblogs, setAllblogs] = useState([]);
+  const [token, setToken] = useState(null);
 
-    const [allblogs, setAllblogs] = useState([]);
-    const [token, setToken] = useState(null);
-
-
-    const navigate = useNavigate();
-    async function getblogs() {
-        let { data } = await axios.get(`${backendurl}/blogs`)
-        console.log(data)
-        setAllblogs(data.data);
+  const navigate = useNavigate();
+  async function getblogs() {
+    let { data } = await axios.get(`${backendurl}/blogs`);
+    console.log(data);
+    setAllblogs(data.data);
+  }
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
     }
-    useEffect(() => {
-        if (localStorage.getItem("token")) {
-            setToken(localStorage.getItem("token"))
-        };
-        getblogs()
-    }, []);
+    getblogs();
+  }, []);
 
-    return (
-        <div>
-            <nav className="navbar">
-                {token ? <button onClick={() => localStorage.removeItem("token")}>Log out</button> : <div><button onClick={() => navigate(`/register`)}  >Register</button>
-                    <button onClick={() => navigate(`/login`)}>Login</button></div>}
-
-            </nav>
-            <main>
-                {allblogs.map((each) => {
-                    return (<div key={each._id} style={{ border: "2px solid tomato" }} onClick={() => navigate(`/blogpage/${each._id}`, { state: each })} >{each.title}</div>)
-                })}
-            </main>
-
-
-        </div>
-    )
+  return (
+    <div>
+      <nav className="navbar">
+        {token ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("token");
+              window.location.reload();
+            }}
+          >
+            Log out
+          </button>
+        ) : (
+          <div>
+            <button onClick={() => navigate(`/register`)}>Register</button>
+            <button onClick={() => navigate(`/login`)}>Login</button>
+          </div>
+        )}
+      </nav>
+      <main>
+        {allblogs.map((each) => {
+          return (
+            <div className="container" key={each._id}>
+              <div
+                className="card"
+                
+                onClick={() =>
+                  navigate(`/blogpage/${each._id}`, { state: each })
+                }
+              >
+                <div className="card__header">
+                  <img
+                    src={each.blogFile}
+                    className="card__image"
+                    width="600"
+                  />
+                </div>
+                <div className="card__body">
+                  <h4>{each.title}</h4>
+                  {/* <p>{each.description}</p> */}
+                </div>
+              </div>
+              </div>
+          );
+        })}
+      </main>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
